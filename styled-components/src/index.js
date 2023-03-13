abe7ad3eb88397c8298dc19faf274afdf170075f
EUNSEO814 <eunseo814@gmail.com>
2023-03-13 18:27:29 +0900

    ⚡️ Typescript

--- styled-components/src/index.js
+++ styled-components/src/index.js
@@ -12,6 +12,7 @@ const lightTheme = {
   backgroundColor: "whitesmoke",
 };
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={darkTheme}>
--- typescript/src/App.tsx
+++ typescript/src/App.tsx
@@ -1,25 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import Circle from "./Circle";
+import styled from "styled-components";
+import { useState } from "react";
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+
+interface DummyProps {
+  text: string;
+  active?: boolean;
+}
+
+const Dummy = ({ text, active = false }: DummyProps) => {
+  return <H1>{text}</H1>;
+};
 
 function App() {
+  const [value, setValue] = useState("");
+  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
+    // console.log(e.currentTarget.value);
+    const {
+      currentTarget: { value },
+    } = e;
+    setValue(value);
+  };
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("hello", value);
+  };
+
+  const onClick = (e: React.MouseEvent<HTMLButtonElement>) => {};
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div>
+    //   <Circle borderColor="yellow" bgColor="teal" />
+    //   <Circle text="i'm here" bgColor="tomato" />
+    // </div>
+    <>
+      <form onSubmit={onSubmit}>
+        <input
+          value={value}
+          onChange={onChange}
+          type="text"
+          placeholder="username"
+        />
+        <button>Log in</button>
+      </form>
+      <Container>
+        <Dummy text="hello" />
+        <button onClick={onClick}>Click Me!</button>
+      </Container>
+    </>
   );
 }
 
