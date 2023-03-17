785410fd51391990c99973d350724385a1d96c0e
EUNSEO814 <eunseo814@gmail.com>
2023-03-17 20:29:10 +0900

    ⚡️ add type

--- crypto-tracker/src/App.tsx
+++ crypto-tracker/src/App.tsx
@@ -1,28 +1,66 @@
-import styled from "styled-components";
-const Container = styled.div`
-  background-color: ${(props) => props.theme.bgColor};
-`;
-const H1 = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
+import { createGlobalStyle } from "styled-components";
+import Router from "./Router";
 
-interface DummyProps {
-  text: string;
-  active?: boolean;
+const GlobalStyle = createGlobalStyle`
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed, 
+figure, figcaption, footer, header, hgroup, 
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+	margin: 0;
+	padding: 0;
+	border: 0;
+	font-size: 100%;
+	font: inherit;
+	vertical-align: baseline;
 }
 
-const Dummy = ({ text, active = false }: DummyProps) => {
-  return <H1>{text}</H1>;
-};
-
+article, aside, details, figcaption, figure, 
+footer, header, hgroup, menu, nav, section {
+	display: block;
+}
+*{
+  box-sizing: border-box;
+}
+body {
+	line-height: 1;
+  font-family: 'Source Sans Pro', sans-serif;
+  background-color: ${(props) => props.theme.bgColor};
+  color: ${(props) => props.theme.textColor};
+}
+ol, ul {
+	list-style: none;
+}
+blockquote, q {
+	quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+	content: '';
+	content: none;
+}
+table {
+	border-collapse: collapse;
+	border-spacing: 0;
+}
+a{
+  text-decoration: none;
+  color: inherit;
+}
+`;
 function App() {
-  const onClick = (e: React.MouseEvent<HTMLButtonElement>) => {};
   return (
     <>
-      <Container>
-        <Dummy text="hello" />
-        <button onClick={onClick}>Click Me!</button>
-      </Container>
+      <GlobalStyle />
+      <Router />
     </>
   );
 }
