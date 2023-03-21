61ba3d316681fe27d3bcbd6333c7238c525584e6
EUNSEO814 <eunseo814@gmail.com>
2023-03-21 23:45:28 +0900

    ⚡️ react-hook-form 알아보기

--- todolist/src/ToDoList.tsx
+++ todolist/src/ToDoList.tsx
@@ -1,25 +1,128 @@
-import { useState } from "react";
-
+// import { useState } from "react";
+import { useForm } from "react-hook-form";
+interface IForm {
+  email: string;
+  firstName: string;
+  lastName: string;
+  username: string;
+  password: string;
+  password1: string;
+  extraError?: string;
+}
 const ToDoList = () => {
-  const [toDo, setToDo] = useState("");
-  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = e;
-    setToDo(value);
+  // setError => 발생하는 문제에 따라 추가적으로 에러를 설정할 수 있게 해줌.
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+  } = useForm<IForm>({
+    defaultValues: {
+      email: "@naver.com",
+    },
+  });
+  const onValid = (data: IForm) => {
+    if (data.password !== data.password1) {
+      setError(
+        "password1",
+        { message: "password are not the same" },
+        { shouldFocus: true }
+      );
+    }
+    // 특정항목이 아니라 전체 form에 해당하는 에러.
+    // setError("extraError", { message: "server offline." });
   };
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log(toDo);
-  };
+  console.log(errors);
+
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input onChange={onChange} value={toDo} placeholder="Write a to do" />
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={handleSubmit(onValid)}
+      >
+        <input
+          {...register("email", {
+            required: "Email is required",
+            pattern: {
+              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
+              message: "Only naver.con emails allowed",
+            },
+          })}
+          placeholder="Email"
+          style={{ borderColor: errors?.email?.message ? "red" : "" }}
+        />
+        <span>{errors?.email?.message}</span>
+        <input
+          {...register("firstName", {
+            required: "write here",
+            validate: {
+              noNico: (value) =>
+                value.includes("nico") ? "no nicos allowed" : true,
+              noNick: (value) =>
+                value.includes("nick") ? "no nick allowed" : true,
+            },
+          })}
+          placeholder="First Name"
+          style={{ borderColor: errors?.firstName?.message ? "red" : "" }}
+        />
+        <span>{errors?.firstName?.message}</span>
+        <input
+          {...register("lastName", { required: "write here" })}
+          placeholder="Last Name"
+          style={{ borderColor: errors?.lastName?.message ? "red" : "" }}
+        />
+        <span>{errors?.lastName?.message}</span>
+        <input
+          {...register("username", { required: "write here", minLength: 10 })}
+          placeholder="Username"
+          style={{ borderColor: errors?.username?.message ? "red" : "" }}
+        />
+        <span>{errors?.username?.message}</span>
+        <input
+          {...register("password", { required: "write here", minLength: 5 })}
+          placeholder="Password"
+          style={{ borderColor: errors?.password?.message ? "red" : "" }}
+        />
+        <span>{errors?.password?.message}</span>
+        <input
+          {...register("password1", {
+            required: "Password is required",
+            minLength: {
+              value: 5,
+              message: "Your password is too short.",
+            },
+          })}
+          placeholder="Password1"
+          style={{ borderColor: errors?.password1?.message ? "red" : "" }}
+        />
+        <span>{errors?.password1?.message}</span>
         <button>Add</button>
+        <span>{errors?.extraError?.message}</span>
       </form>
     </div>
   );
 };
+
+// const ToDoList = () => {
+//   const [toDo, setToDo] = useState("");
+//   const onChange = (e: React.FormEvent<HTMLInputElement>) => {
+//     const {
+//       currentTarget: { value },
+//     } = e;
+//     setToDo(value);
+//   };
+//   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+//     e.preventDefault();
+//     console.log(toDo);
+//   };
+//   return (
+//     <div>
+//       <form onSubmit={onSubmit}>
+//         <input onChange={onChange} value={toDo} placeholder="Write a to do" />
+//         <button>Add</button>
+//       </form>
+//     </div>
+//   );
+// };
 
 export default ToDoList;
